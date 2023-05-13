dab5ff609e4bac25e7aeb8d697efbd2fa181d4ba
Finnomator <finndruenert@gmail.com>
2023-05-13 14:39:12 +0200

    Pause Menü
    (cherry picked from commit b38404c0f4e1800ba995b7fb507350364db6a0b7)

--- src/main/java/cardnight/MainMenuView.java
+++ src/main/java/cardnight/MainMenuView.java
@@ -3,10 +3,18 @@ package cardnight;
 import cardnight.games.tictactoe.TicTacToeView;
 import cardnight.games.ueno.viewcontroler.UenoView;
 import cardnight.games.witch.WitchView;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
 public class MainMenuView {
+
+    public Slider soundVolumeSlider;
+
+    public void initialize() {
+        soundVolumeSlider.setValue(Main.SOUND_VOLUME);
+    }
 
     public void onTicTacToeClick() throws IOException {
         ScreenController.activateNewPane(TicTacToeView.loadScene());
@@ -22,5 +30,10 @@ public class MainMenuView {
 
     public void onUenoClick() throws IOException {
         ScreenController.activateNewPane(UenoView.loadScene());
+    }
+
+    public void onSoundSliderMouseClick() {
+        Main.SOUND_VOLUME = soundVolumeSlider.getValue();
+        System.out.println("Sound bei " + Main.SOUND_VOLUME + "%");
     }
 }
--- src/main/java/cardnight/games/tictactoe/TicTacToeView.java
+++ src/main/java/cardnight/games/tictactoe/TicTacToeView.java
@@ -1,17 +1,26 @@
 package cardnight.games.tictactoe;
 
+import cardnight.PauseMenu;
 import cardnight.ScreenController;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 
 public class TicTacToeView {
-    public void onZurueckClick() {
-        ScreenController.activate("main-menu-view");
+    public StackPane root;
+
+    public void initialize() {
+
     }
 
     public static Pane loadScene() throws IOException {
         return new FXMLLoader(TicTacToeView.class.getResource("/cardnight/game-views/tictactoe-view.fxml")).load();
+    }
+
+    public void onPauseClick() throws IOException {
+        root.getChildren().add(PauseMenu.loadScene());
     }
 }

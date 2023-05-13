d32a19760f0bdc65dcda7f31d467fe55b7ac4538
Finnomator <finndruenert@gmail.com>
2023-05-13 13:19:44 +0200

    witch view

--- src/main/java/cardnight/games/witch/WitchView.java
+++ src/main/java/cardnight/games/witch/WitchView.java
@@ -1,26 +1,24 @@
 package cardnight.games.witch;
 
 import cardnight.ScreenController;
-import cardnight.games.ueno.Ueno;
-import cardnight.games.ueno.viewcontroler.UenoGegnerUiHand;
-import cardnight.games.ueno.viewcontroler.UenoHauptspielerUiHand;
-import cardnight.games.ueno.viewcontroler.UenoKarteKlickEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 public class WitchView {
-    public void onBackClick() {
-        ScreenController.activate("main-menu-view");
+
+    public void initialize() {
+        System.out.println("Witch initiiert");
+        // Von hier aus wird die ganze Logik gestartet
     }
 
     public static Pane loadScene() throws IOException {
         return new FXMLLoader(cardnight.games.witch.WitchView.class.getResource("/cardnight/game-views/witch-view.fxml")).load();
+    }
+
+    public void onZurueckClick() {
+        ScreenController.activate("main-menu-view");
     }
 }
 

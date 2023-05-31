761e7603480899bba61ac3f3fe073495121bdfe2
Finnomator <finndruenert@gmail.com>
2023-05-31 12:02:11 +0200

    SpieleBilder

---
+++ src/main/java/cardnight/games/SpieleBilder.java
@@ -0,0 +1,13 @@
+package cardnight.games;
+
+import cardnight.Main;
+import javafx.scene.image.Image;
+
+public class SpieleBilder {
+
+    public static final Image stickman = new Image(Main.class.getResourceAsStream(
+            "/cardnight/game-views/images/stickman.png"),
+            255, 308, true, true
+    );
+
+}

be552e7c82526942fc9f785232b28937f227961d
Finnomator <finndruenert@gmail.com>
2023-06-06 16:09:46 +0200

    Größe der Punktetafel reduziert

--- src/main/java/cardnight/games/witch/viewcontroller/WitchPunktetafel.java
+++ src/main/java/cardnight/games/witch/viewcontroller/WitchPunktetafel.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class WitchPunktetafel {
@@ -40,9 +41,12 @@ public class WitchPunktetafel {
 
             for (int zeile = 0; zeile < spiel.gibRundenNummer(); ++zeile) {
                 Text uiText = new Text(String.valueOf(s.gibPunkte(zeile)));
+                uiText.setFont(new Font(9));
                 GridPane.setHalignment(uiText, HPos.CENTER);
                 root.add(uiText, (spalte + 1) * 4, (zeile + 1) * 2);
-                root.add(new Text(String.valueOf(s.gibStichSchaetzung(zeile))), (spalte + 1) * 4 + 2, (zeile + 1) * 2);
+                Text text = new Text(String.valueOf(s.gibStichSchaetzung(zeile)));
+                text.setFont(new Font(9));
+                root.add(text, (spalte + 1) * 4 + 2, (zeile + 1) * 2);
             }
         }
 
@@ -55,8 +59,11 @@ public class WitchPunktetafel {
     }
 
     private void bezeichnungenEintragen(int anzahlZeilen) {
-        for (int zeile = 0; zeile < anzahlZeilen; ++zeile)
-            root.add(new Text(String.valueOf(zeile + 1)), 0, (zeile + 1) * 2);
+        for (int zeile = 0; zeile < anzahlZeilen; ++zeile) {
+            Text text = new Text(String.valueOf(zeile + 1));
+            text.setFont(new Font(9));
+            root.add(text, 0, (zeile + 1) * 2);
+        }
 
         root.add(new Text("Total"), 0, (anzahlZeilen + 1) * 2);
 
@@ -70,13 +77,13 @@ public class WitchPunktetafel {
 
         AnchorPane pane = new AnchorPane();
         pane.setStyle("-fx-background-color: black");
-        pane.setPrefWidth(2);
+        pane.setPrefWidth(1);
         GridPane.setRowSpan(pane, Integer.MAX_VALUE);
         root.add(pane, 1, 0);
 
         AnchorPane pane2 = new AnchorPane();
         pane2.setStyle("-fx-background-color: black");
-        pane2.setPrefHeight(2);
+        pane2.setPrefHeight(1);
         GridPane.setColumnSpan(pane2, Integer.MAX_VALUE);
         root.add(pane2, 0, 1);
 
@@ -85,7 +92,7 @@ public class WitchPunktetafel {
             for (int i : is) {
                 AnchorPane pane1 = new AnchorPane();
                 pane1.setStyle("-fx-background-color: black");
-                pane1.setPrefWidth(2);
+                pane1.setPrefWidth(1);
                 GridPane.setRowSpan(pane1, anzahlZeilen * 2 + (i + 1));
                 root.add(pane1, (spalte + 1) * 4 + i, 0);
             }
@@ -94,7 +101,7 @@ public class WitchPunktetafel {
         for (int zeile = 0; zeile < anzahlZeilen; ++zeile) {
             AnchorPane pane1 = new AnchorPane();
             pane1.setStyle("-fx-background-color: black");
-            pane1.setPrefHeight(2);
+            pane1.setPrefHeight(1);
             GridPane.setColumnSpan(pane1, Integer.MAX_VALUE);
             root.add(pane1, 0, (zeile + 1) * 2 + 1);
         }

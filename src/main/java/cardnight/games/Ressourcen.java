849486983e2e44478a39d615e1fadad0713d37c1
Finnomator <finndruenert@gmail.com>
2023-06-08 12:44:38 +0200

    Stickman ressourcen

---
+++ src/main/java/cardnight/games/Ressourcen.java
@@ -0,0 +1,9 @@
+package cardnight.games;
+
+import cardnight.Main;
+import javafx.scene.image.Image;
+
+public class Ressourcen {
+    public static final Image stickmanImage = new Image(Main.class.getResourceAsStream("/cardnight/game-views/images/stickman.png"));
+    public static final Image stickmanHappyImage = new Image(Main.class.getResourceAsStream("/cardnight/game-views/images/stickman-happy.png"));
+}

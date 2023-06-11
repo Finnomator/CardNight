a3749b5de0197698122cf8ebebce98e427a0a51d
Finnomator <finndruenert@gmail.com>
2023-06-11 21:39:13 +0200

    Drehende Karten im Hauptmenü

--- src/main/java/cardnight/MainMenuView.java
+++ src/main/java/cardnight/MainMenuView.java
@@ -1,18 +1,28 @@
 package cardnight;
 
 import cardnight.games.tictactoe.viewcontroller.TTTGegnerWahl;
+import cardnight.games.ueno.UenoFarbe;
+import cardnight.games.ueno.UenoKarte;
+import cardnight.games.ueno.viewcontroler.UenoKartenBilder;
 import cardnight.games.ueno.viewcontroler.UenoView;
+import cardnight.games.witch.WitchFarbe;
+import cardnight.games.witch.WitchKarte;
+import cardnight.games.witch.viewcontroller.WitchKartenBilder;
 import cardnight.games.witch.viewcontroller.WitchView;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
-import sun.security.provider.ConfigFile;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Random;
 
 public class MainMenuView {
 
@@ -22,11 +32,18 @@ public class MainMenuView {
     public ImageView uenoButtonImgView;
     public ImageView witchButtonImgView;
     public ImageView beendenButtonImgView;
+    public ImageView linkeSpinKarte;
+    public ImageView rechteSpinKarte;
 
     public void initialize() {
         MainMenuBilder.ladeBilder();
+        UenoKartenBilder.ladeBilder();
+        WitchKartenBilder.bilderLaden();
         hintergrundImageView.fitWidthProperty().bind(root.widthProperty().subtract(500));
+        linkeSpinKarte.fitHeightProperty().bind(root.heightProperty().divide(2.5));
+        rechteSpinKarte.fitHeightProperty().bind(root.heightProperty().divide(2.5));
         setzeHintergrund(MainMenuBilder.gibDefaultHintergrund());
+        startRotatingCards();
     }
 
     public void onTicTacToeClick() throws IOException {
@@ -72,31 +89,37 @@ public class MainMenuView {
     public void mouseEnteredTTT() {
         setzeHintergrund(MainMenuBilder.gibHintergrundBild(SpielTyp.TIC_TAC_TOE));
         tttButtonImgView.setImage(MainMenuBilder.gibButtonBild(SpielTyp.TIC_TAC_TOE, true));
+        zeigeSpinKarten(false);
     }
 
     public void mouseExitedTTT() {
         setzeHintergrund(MainMenuBilder.gibDefaultHintergrund());
         tttButtonImgView.setImage(MainMenuBilder.gibButtonBild(SpielTyp.TIC_TAC_TOE, false));
+        zeigeSpinKarten(true);
     }
 
     public void mouseExitedUeno() {
         setzeHintergrund(MainMenuBilder.gibDefaultHintergrund());
         uenoButtonImgView.setImage(MainMenuBilder.gibButtonBild(SpielTyp.UENO, false));
+        zeigeSpinKarten(true);
     }
 
     public void mouseEnteredWitch() {
         setzeHintergrund(MainMenuBilder.gibHintergrundBild(SpielTyp.WITCH));
         witchButtonImgView.setImage(MainMenuBilder.gibButtonBild(SpielTyp.WITCH, true));
+        zeigeSpinKarten(false);
     }
 
     public void mouseEnteredUeno() {
         setzeHintergrund(MainMenuBilder.gibHintergrundBild(SpielTyp.UENO));
         uenoButtonImgView.setImage(MainMenuBilder.gibButtonBild(SpielTyp.UENO, true));
+        zeigeSpinKarten(false);
     }
 
     public void mouseExitedWitch() {
         setzeHintergrund(MainMenuBilder.gibDefaultHintergrund());
         witchButtonImgView.setImage(MainMenuBilder.gibButtonBild(SpielTyp.WITCH, false));
+        zeigeSpinKarten(true);
     }
 
     public void mouseEnteredBeenden() {
@@ -105,5 +128,70 @@ public class MainMenuView {
 
     public void mouseExitedBeenden() {
         beendenButtonImgView.setImage(MainMenuBilder.beendenButtonBild(false));
+    }
+
+    private void startRotatingCards() {
+
+        Rotate linkeYRotation = new Rotate(0, Rotate.Y_AXIS);
+        Rotate rechteYRotation = new Rotate(0, Rotate.Y_AXIS);
+
+        Rotate linkeZRotation = new Rotate(-34.66, Rotate.Z_AXIS);
+        Rotate rechteZRotation = new Rotate(34.66, Rotate.Z_AXIS);
+
+        linkeZRotation.pivotXProperty().bind(linkeSpinKarte.fitHeightProperty().divide(2).divide(1.447));
+        linkeZRotation.pivotYProperty().bind(linkeSpinKarte.fitHeightProperty().divide(2));
+        linkeYRotation.pivotXProperty().bind(linkeSpinKarte.fitHeightProperty().divide(2).divide(1.447));
+        linkeYRotation.pivotYProperty().bind(linkeSpinKarte.fitHeightProperty().divide(2));
+
+        rechteZRotation.pivotXProperty().bind(rechteSpinKarte.fitHeightProperty().divide(2).divide(1.447));
+        rechteZRotation.pivotYProperty().bind(rechteSpinKarte.fitHeightProperty().divide(2));
+        rechteYRotation.pivotXProperty().bind(rechteSpinKarte.fitHeightProperty().divide(2).divide(1.447));
+        rechteYRotation.pivotYProperty().bind(rechteSpinKarte.fitHeightProperty().divide(2));
+
+        linkeSpinKarte.getTransforms().addAll(linkeYRotation, linkeZRotation);
+        rechteSpinKarte.getTransforms().addAll(rechteYRotation, rechteZRotation);
+
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(10), actionEvent -> {
+
+            int winkel = (int) Math.abs(linkeYRotation.getAngle()) % 360;
+
+            if (winkel == 90 || winkel == 270) {
+                linkeSpinKarte.setImage(randomKartenBild());
+                rechteSpinKarte.setImage(randomKartenBild());
+            }
+
+            linkeYRotation.setAngle(linkeYRotation.getAngle() - 1);
+            rechteYRotation.setAngle(rechteYRotation.getAngle() + 1);
+        }));
+        timeline.play();
+    }
+
+    private static Image randomKartenBild() {
+
+        Random rnd = new Random();
+
+        int rndGame = rnd.nextInt(2);
+
+        switch (rndGame) {
+            case 0:
+                return UenoKartenBilder.karteZuBild(new UenoKarte(
+                        rnd.nextInt(10),
+                        UenoFarbe.values()[rnd.nextInt(UenoFarbe.values().length)]
+                ));
+            case 1:
+                return WitchKartenBilder.karteZuBild(new WitchKarte(
+                        WitchFarbe.values()[rnd.nextInt(WitchFarbe.values().length)],
+                        rnd.nextInt(15)
+                ));
+        }
+
+        throw new RuntimeException();
+    }
+
+    private void zeigeSpinKarten(boolean sichtbar) {
+        linkeSpinKarte.setVisible(sichtbar);
+        rechteSpinKarte.setVisible(sichtbar);
     }
 }

792945e0506beef226ac076b89e021b4445117a5
Finnomator <finndruenert@gmail.com>
2023-06-12 13:38:06 +0200

    Direkte java implementation der Tic Tac Toe Karten

--- src/main/java/cardnight/games/tictactoe/viewcontroller/TTTUiOHand.java
+++ src/main/java/cardnight/games/tictactoe/viewcontroller/TTTUiOHand.java
@@ -1,13 +1,22 @@
 package cardnight.games.tictactoe.viewcontroller;
 
+import cardnight.Main;
 import cardnight.games.tictactoe.TicTacToeSpieler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 import java.io.IOException;
 
 public class TTTUiOHand extends TTTUiHand {
     public HBox kartenBox;
+
+    public static final Image oHandkartenBild = new Image(Main.class.getResourceAsStream(
+            "/cardnight/game-views/tictactoe/images/O_Handkarte.png"),
+            Main.HANDKARTE_BREITE, 0, true, true);
 
     @Override
     public void uiErstellen(TicTacToeSpieler spieler) {
@@ -23,13 +32,11 @@ public class TTTUiOHand extends TTTUiHand {
         kartenBox.getChildren().clear();
 
         for (int i = 0; i < spieler.gibAnzahlHandKarten(); ++i) {
-            FXMLLoader loader;
-            loader = new FXMLLoader(getClass().getResource("/cardnight/game-views/tictactoe/o-karte.fxml"));
-            try {
-                kartenBox.getChildren().add(loader.load());
-            } catch (IOException ex) {
-                throw new RuntimeException();
-            }
+            Button btn = new Button();
+            btn.setPadding(new Insets(0.001));
+            btn.getStylesheets().add(getClass().getResource("/cardnight/transparent-image-button.css").toExternalForm());
+            btn.setGraphic(new ImageView(oHandkartenBild));
+            kartenBox.getChildren().add(btn);
         }
     }
 }

799965817515dd39bca493006dc364969eb0f25b
Finnomator <finndruenert@gmail.com>
2023-06-15 23:02:37 +0200

    Nochmal spielen im Game Over screen

--- src/main/java/cardnight/GameOver.java
+++ src/main/java/cardnight/GameOver.java
@@ -1,6 +1,11 @@
 package cardnight;
 
+import cardnight.games.tictactoe.TicTacToe;
+import cardnight.games.tictactoe.viewcontroller.TicTacToeView;
+import cardnight.games.ueno.Ueno;
 import cardnight.games.ueno.viewcontroler.UenoView;
+import cardnight.games.witch.Witch;
+import cardnight.games.witch.viewcontroller.WitchView;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -20,11 +25,22 @@ public class GameOver {
     }
 
     public static StackPane loadScene() throws IOException {
-        return new FXMLLoader(UenoView.class.getResource("/cardnight/game-over.fxml")).load();
+        return new FXMLLoader(Main.class.getResource("/cardnight/game-over.fxml")).load();
     }
 
     public void onZumHauptmenueClick() {
         SoundPlayer.klickSound();
         ScreenController.activate("main-menu-view");
+    }
+
+    public void onNochmalSpielenClick() throws IOException {
+        SoundPlayer.klickSound();
+
+        if (Main.gibAktuellGespieltesSpiel() instanceof TicTacToe)
+            ScreenController.activateNewPane(TicTacToeView.loadScene());
+        else if (Main.gibAktuellGespieltesSpiel() instanceof Ueno)
+            ScreenController.activateNewPane(UenoView.loadScene());
+        else if (Main.gibAktuellGespieltesSpiel() instanceof Witch)
+            ScreenController.activateNewPane(WitchView.loadScene());
     }
 }

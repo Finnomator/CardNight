1f2ee1865b36b381be9d5fdd3103cb6709fd5de5
Finnomator <finndruenert@gmail.com>
2023-06-15 23:23:13 +0200

    Möglichkeit für neues Spiel im Pausenmenü

--- src/main/java/cardnight/PauseMenu.java
+++ src/main/java/cardnight/PauseMenu.java
@@ -1,6 +1,13 @@
 package cardnight;
 
 import cardnight.games.Spiel;
+import cardnight.games.tictactoe.TicTacToe;
+import cardnight.games.tictactoe.viewcontroller.TicTacToeView;
+import cardnight.games.ueno.Ueno;
+import cardnight.games.ueno.viewcontroler.UenoView;
+import cardnight.games.witch.Witch;
+import cardnight.games.witch.viewcontroller.WitchView;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -34,4 +41,14 @@ public class PauseMenu {
         return new FXMLLoader(Main.class.getResource("/cardnight/pause-menu.fxml")).load();
     }
 
+    public void onErneutSpielenClick() throws IOException {
+        SoundPlayer.klickSound();
+
+        if (Main.gibAktuellGespieltesSpiel() instanceof TicTacToe)
+            ScreenController.activateNewPane(TicTacToeView.loadScene());
+        else if (Main.gibAktuellGespieltesSpiel() instanceof Ueno)
+            ScreenController.activateNewPane(UenoView.loadScene());
+        else if (Main.gibAktuellGespieltesSpiel() instanceof Witch)
+            ScreenController.activateNewPane(WitchView.loadScene());
+    }
 }
